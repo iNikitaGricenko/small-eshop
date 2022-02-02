36e02b4e03b7b2609aef6c7178d043aa4e31d572
iNikitaGricenko <VfVristalt@gmail.com>
2022-02-02 14:34:24 +0200

    thymeleaf mail

--- src/main/java/com/example/eshop/service/MailService.java
+++ src/main/java/com/example/eshop/service/MailService.java
@@ -1,30 +1,35 @@
 package com.example.eshop.service;
 
+import com.example.eshop.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.internet.MimeMessage;
 
 @Service
 @RequiredArgsConstructor
 public class MailService {
+
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
-    @Async
     @SneakyThrows
-    public void send(String email, String subject, String message) {
+    public void send(User user) {
+        Context context = new Context();
+        context.setVariable("User", user);
+
+        String proccess = templateEngine.process("mail/verification_mail", context);
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-
         MimeMessageHelper helper =  new MimeMessageHelper(mimeMessage, "UTF-8");
-
         helper.setFrom("hello@eshop.com");
-        helper.setTo(email);
-        helper.setSubject(subject);
-        helper.setText(message, true);
+        helper.setTo(user.getEmail());
+        helper.setSubject("Activation code");
+        helper.setText(proccess, true);
 
         mailSender.send(mimeMessage);
     }
